c3e17dd04525d557b09cfab61a6ba0c115a2eca6
Mirko Cukich <mirkocukich@gmail.com>
2019-09-02 22:38:17 -0500

    Initial Commit

---
+++ Table View Sect Group Cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View Sect Group Cell
+//
+//  Created by Mirko Cukich on 9/2/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
